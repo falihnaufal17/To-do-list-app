1a96374f43360cd9b61281a6707eb073a8d13759
falihnaufal17 <dudu.kotkot@gmail.com>
2019-09-17 14:51:56 +0700

    fix bugs

--- src/controllers/todolist.js
+++ src/controllers/todolist.js
@@ -28,7 +28,7 @@ module.exports = {
             })
     },
     addTodo: (req, res) => {
-        let dateNow = moment(new Date().getTime()).format('HH:mm')
+        let dateNow = moment(new Date().getTime()).format('HH:mm:ss')
         const data = {
             nama_peserta: req.body.nama_peserta,
             kegiatan: req.body.kegiatan,
